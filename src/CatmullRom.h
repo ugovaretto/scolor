992f1970cd16317b1d50aab06406ee2beade3be8
uvaretto <uvaretto@Lodrino.local>
2014-06-26 09:49:03 +0200

    Fixed parameter ordering in call to crom interpolation function

--- src/CatmullRom.h
+++ src/CatmullRom.h
@@ -10,6 +10,7 @@
 #include "Vector3D.h"
 
 //------------------------------------------------------------------------------
+///Compute at each point the sum of the distences from the first point
 template < typename FwdIt >
 std::vector< typename std::iterator_traits< FwdIt >::value_type > 
 ComputeDistances(FwdIt begin, FwdIt end) {
@@ -27,8 +28,10 @@ ComputeDistances(FwdIt begin, FwdIt end) {
     return res;
 }
 
+///Find in the sorted array of distances the position of the element holding
+///the closest value lower or equal to the passed value 
 template < typename ScalarT >
-std::size_t FindPos(const std::vector< ScalarT > dist, ScalarT t) {
+std::size_t FindPos(const std::vector< ScalarT >& dist, ScalarT t) {
     assert(t >= ScalarT(0) && t <= ScalarT(1));
     const ScalarT totalSize = dist.back();
     const ScalarT d = t * totalSize;
@@ -38,34 +41,45 @@ std::size_t FindPos(const std::vector< ScalarT > dist, ScalarT t) {
     return std::size_t(std::distance(dist.begin(), e));
 }
 
+///Extract normalized ([0,1]) parameter and control points from global control
+///point array.
+///The point array *shall* have a size = dist.size + 4 because:
+/// # two points are added to the original array to allow for proper
+//interpolation
+/// # the first and last two points are replicated and the first point
+///   meaningful for interpolation is the secon one in a CR spline
 template < typename ScalarT >
-std::tuple< const Vector3D< ScalarT >&,
+std::tuple< ScalarT,
             const Vector3D< ScalarT >&,
             const Vector3D< ScalarT >&,
             const Vector3D< ScalarT >&,
-            ScalarT >
+            const Vector3D< ScalarT >& >
 Extract(const std::vector< ScalarT >& dist,
         const std::vector< Vector3D< ScalarT > >& points,//size = dist.size + 4,
         ScalarT t) {
    using V = Vector3D< ScalarT >;
-   using T = std::tuple< const V&, const V&, const V&, const V&, ScalarT >;
+   using T = std::tuple< ScalarT, const V&, const V&, 
+                         const V&, const V&, ScalarT >;
    const std::size_t p = FindPos(dist, t);
-   const std::size_t pidx1 = p + 2;
+   const std::size_t pidx1 = p + 2; //move to second point where
+                                    //first point is replicated
    const std::size_t pidx0 = pidx1 - 1;
    const std::size_t pidx2 = pidx1 + 1;
-   const std::size_t pidx3 = pidx3 + 1;
+   const std::size_t pidx3 = pidx2 + 1;
    const ScalarT u = (t * dist.back() - dist[p]) 
                      / Dist(points[pidx1], points[pidx2]);
-   return T(points[pidx0], points[pidx1], points[pidx2], points[pidx3], u);
+   return T(u, points[pidx0], points[pidx1], points[pidx2], points[pidx3]);
 }
 
 //------------------------------------------------------------------------------
+///Standard Catmull-Rom spline
 template < typename ScalarT >
 Vector3D< ScalarT > CatmullRom(ScalarT u,
                                const Vector3D< ScalarT >& P0,
                                const Vector3D< ScalarT >& P1,
                                const Vector3D< ScalarT >& P2,
                                const Vector3D< ScalarT >& P3) {
+    assert(u >= ScalarT(0) && u <= Scalart(1));
     const Vector3D< ScalarT > c0(P1);
     const Vector3D< ScalarT > c1 = -0.5 * P0 + 0.5 * P2;
     const Vector3D< ScalarT > c2 = P0 - 2.5 * P1 + 2.0 * P2 - 0.5 * P3;
@@ -74,6 +88,7 @@ Vector3D< ScalarT > CatmullRom(ScalarT u,
 }
 
 //------------------------------------------------------------------------------
+///Piecewise Catmull Rom spline
 template < typename ScalarT > Vector3D< ScalarT >
 CRomInterpolation(const std::vector< Vector3D< ScalarT > >& points,
                   const std::vector< ScalarT >& dist,
@@ -84,12 +99,16 @@ CRomInterpolation(const std::vector< Vector3D< ScalarT > >& points,
     assert(points.size() == dist.size() + 4);
     using V = Vector3D< ScalarT >;
     using T = std::tuple< const V&, const V&, const V&, const V&, ScalarT >; 
-    const T p = Extract(dist, points, t);
-    return CatmullRom(t, std::get<0>(p), 
+    const T p(Extract(dist, points, t));
+    return CatmullRom(std::get<0>(p), 
                       std::get<1>(p), std::get<2>(p), std::get<3>(p), 
                       std::get<4>(p));
 }
 //------------------------------------------------------------------------------
+///Keyframed interpolation: each parameter value in the keys array matches
+///a point in the points array
+///keys and arrays must be of the same size and shall contain all the points
+///in the original vector with the first and last points replicated
 template < typename ScalarT > Vector3D< ScalarT >
 KeyFramedCRomInterpolation(const std::vector< Vector3D< ScalarT > >& points,
                            const std::vector< ScalarT >& keys,
@@ -104,7 +123,10 @@ KeyFramedCRomInterpolation(const std::vector< Vector3D< ScalarT > >& points,
     typename V::const_iterator j = i;
     ++j;
     const ScalarT u = (t - *i) / (*j - *i);
-    const std::size_t pidx1 = std::distance(keys.begin(), i);
+    //first point is replicated: if lower bound matches keys[0] we need to add
+    //1
+    const std::size_t d = std::size_t(std::distance(keys.begin(), i));
+    const std::size_t pidx1 = d == 0 ? d + 1 : d;
     const std::size_t pidx0 = pidx1 - 1;
     const std::size_t pidx2 = pidx1 + 1;
     const std::size_t pidx3 = pidx2 + 1;
